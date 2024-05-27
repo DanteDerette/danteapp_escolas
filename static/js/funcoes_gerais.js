22ddd739289ebfa59eee6d7f60b42b1e02f4e84b
dantederette <dantederette@gmail.com>
2024-05-27 19:18:35 -0300

    Aula 13

--- static/js/funcoes_gerais.js
+++ static/js/funcoes_gerais.js
@@ -1,4 +1,56 @@
 function gera_input(item, parent){
+	
+	var para_inputs_arr = [
+		'text',
+		'number',
+		'date'
+	]
+
+	var para_select_arr = ['select']
+
+	if(para_inputs_arr.includes(item.type)){
+		para_inputs(item, parent)
+	} else if(para_select_arr.includes(item.type)){
+		para_select(item, parent)
+	}
+
+
+
+
+}
+
+function para_select(item, parent){
+	var row = document.createElement('div')
+	row.className = 'row mt-1'
+	
+	var col = document.createElement('div')
+	col.className = 'col'
+	
+	var label = document.createElement('label')
+	label.innerHTML = item.label_innerHTML
+	
+	var select = document.createElement('select')
+	select.name = item.name	
+	select.className = 'form-select'
+	select.classList.add(item.input_class)
+	col.appendChild(label)
+	col.appendChild(select)
+
+	item.opts_arr.forEach((
+		opt, index
+	) => {
+		var option = document.createElement('option')
+		option.innerHTML = item.opt_arr_innerHTML[index]
+		option.value = opt
+		select.appendChild(option)
+	})
+
+	
+	row.appendChild(col)
+	$(parent).append(row)
+}
+
+function para_inputs(item, parent){
 	var row = document.createElement('div')
 	row.className = 'row mt-1'
 	
