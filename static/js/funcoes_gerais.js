f51efc39be96a7a58556e20c5ac45d2f0c7d77ab
DanteDerette <sdanted@hotmail.com>
2024-06-11 09:04:02 -0300

    Aula 24

--- static/js/funcoes_gerais.js
+++ static/js/funcoes_gerais.js
@@ -6,16 +6,62 @@ function gera_input(item, parent){
 		'date'
 	]
 
+	var para_inputs_arr_file = [
+		'file'
+	]
+
 	var para_select_arr = ['select']
 
 	if(para_inputs_arr.includes(item.type)){
 		para_inputs(item, parent)
 	} else if(para_select_arr.includes(item.type)){
 		para_select(item, parent)
+	} else if(para_inputs_arr_file.includes(item.type)){
+		para_inputs_file(item, parent)
 	}
 
+}
 
+function para_inputs_file(item, parent){
+	var row = document.createElement('div')
+	row.className = 'row mt-1'
+	
+	var col = document.createElement('div')
+	col.className = 'col'
+	
+	var label = document.createElement('label')
+	if(item.required){
+		item.label_innerHTML += `
+			<span style='color: red'>*</span>
+		`
+	}
+	label.innerHTML = item.label_innerHTML
+	
+	var input = document.createElement('input')
+	input.type = item.type
+	input.name = item.name	
+	input.accept = "image/*"
+	if(item.disabled){
+		input.disabled = item.disabled
+	}
+	if(item.required){
+		input.required = item.required
+	}
 
+	if(item.func_oninput){
+		input.oninput = (e) => {
+			item.func_oninput(e.currentTarget)
+		}
+	}
+
+	input.className = 'form-control'
+	input.autocomplete = 'one-time-code'
+	input.classList.add(item.input_class)
+	
+	col.appendChild(label)
+	col.appendChild(input)
+	row.appendChild(col)
+	$(parent).append(row)
 
 }
 
